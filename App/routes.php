ee43f6c6d9743d36468720281b89bdf8a5aab606
QuentinBubu <QuentinBubu.dev@gmail.com>
2021-11-23 21:42:52 +0100

    Add routes for login & signup

--- App/routes.php
+++ App/routes.php
@@ -6,6 +6,13 @@ use Bubu\Router\Router;
 $router = new Router(URL);
 
 $router->get('/', 'Home#create');
+
+$router->get('/login', 'Login#create');
+$router->post('/login', 'Login#store');
+
+$router->get('/signup', 'Signup#create');
+$router->post('/signup', 'Signup#store');
+
 $router->get('/admin', 'Admin#create');
 
 $router->run();
