523330a43d9c242c5974957af5884d77b276e334
QuentinBubu <QuentinBubu.dev@gmail.com>
2021-11-23 19:14:24 +0100

    Add Main page controller

---
+++ App/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+
+use App\Views\Page;
+use Bubu\Http\HttpRequire\HttpRequire;
+use Bubu\Http\Session\Session;
+
+class HomeController
+{
+    /**
+     * @return never
+     */
+    public static function create()
+    {
+        HttpRequire::https();
+        (new Page)->show('home');
+    }
+
+    public static function logout()
+    {
+        Session::destroy();
+        header('Location: /');
+        exit;
+    }
+}
